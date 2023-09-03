3d448a7a720040ff4604ae77a2ad3fcc577c9eba
Ty Gutowski <tygutowski@gmail.com>
2023-09-03 10:42:33 -0400

    finished

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+    res.header('Cross-Origin-Embedder-Policy', 'require-corp');
+    res.header('Cross-Origin-Opener-Policy', 'same-origin');
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
